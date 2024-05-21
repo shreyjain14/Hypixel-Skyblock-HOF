428758ca92b33b49537511a4a19ee4685cab5920
Shrey <shreyjain1403@gmail.com>
2024-05-21 17:21:23 +0530

    the new update leaderboard function

--- website/backend/get_api.py
+++ website/backend/get_api.py
@@ -130,4 +130,33 @@ def get_profiles(username):
 
 
 if __name__ == "__main__":
-    get_profiles('Z109')
+    tops = {
+        'skills': {
+            'skyblock_xp': 14200,
+            'farming_xp': 0,
+            'mining_xp': 0,
+            'combat_xp': 0,
+            'foraging_xp': 0,
+            'fishing_xp': 0,
+            'enchanting_xp': 0,
+            'alchemy_xp': 0,
+            'carpentry_xp': 0,
+            'taming_xp': 0,
+            'runecrafting_xp': 0,
+            'social_xp': 0
+        },
+        'slayer': {
+            'zombie_xp': 0,
+            'spider_xp': 0,
+            'wolf_xp': 0,
+            'enderman_xp': 0,
+            'blaze_xp': 0
+        }
+    }
+
+    connection = psycopg2.connect(db_url)
+
+    with connection:
+        with connection.cursor() as cursor:
+
+            update_leaderboard(connection, cursor, tops, 'darkdash')
