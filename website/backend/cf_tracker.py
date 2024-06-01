5e4dd11561803d594e0df7e74b4c56df2a0fe6dc
Shrey <shreyjain1403@gmail.com>
2024-06-01 14:38:47 +0530

    added dupes and dupes increase tab in the tracker

--- website/backend/cf_tracker.py
+++ website/backend/cf_tracker.py
@@ -22,7 +22,7 @@ def get_user_data():
 
     # Query to fetch data for specific usernames
     cursor.execute("""
-        SELECT username, timestramp, chocolate, increase
+        SELECT username, timestramp, chocolate, increase, rabbit_dupes, rabbit_dupes_inc
         FROM cf_tracker
         WHERE username IN %s
         ORDER BY username, timestramp;
@@ -39,12 +39,14 @@ def get_user_data():
     last_chocolates = {username: None for username in usernames}
 
     for row in rows:
-        username, timestramp, chocolate, increase = row
+        username, timestramp, chocolate, increase, dupes, dupes_inc = row
 
         user_data[username].append({
             'time': timestramp,
             'chocolate': chocolate,
-            'increase': increase
+            'increase': increase,
+            'dupes': dupes,
+            'dupes_inc': dupes_inc
         })
 
         last_chocolates[username] = chocolate
@@ -63,20 +65,55 @@ def add_data():
 
     for username in usernames:
 
-        cursor.execute(f"SELECT chocolate FROM cf_tracker WHERE username = '{username}' ORDER BY id DESC LIMIT 1")
+        cursor.execute(f"SELECT chocolate, rabbit_dupes FROM cf_tracker WHERE username = '{username}' ORDER BY id DESC LIMIT 1")
 
         # Fetch the result
-        last_row = cursor.fetchone()[0]
+        last_row = cursor.fetchone()
 
-        response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}").json()
+        reply = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}").json()
 
-        if 'id' in response:
-            chocolate = stranded_api.stranded(response['id'])['misc']['chocolate']
+        if 'id' in reply:
+            headers = {'API-Key': os.getenv('HYPIXEL-API-KEY')}
+            params = {'uuid': reply['id']}
+
+            url = 'https://api.hypixel.net/v2/skyblock/profiles'
+
+            response = requests.get(url, headers=headers, params=params).json()
+
+            profiles = []
+
+            if response['success']:
+                if response['profiles']:
+                    for profile_info in response['profiles']:
+                        if 'game_mode' in profile_info:
+                            if profile_info['game_mode'] == 'island':
+                                profiles.append(profile_info['members'][reply['id']])
+
+            chocolate = 0
+            rabbits_dupes = 0
+
+            for profile in profiles:
+
+                if 'events' in profile:
+                    if 'easter' in profile['events']:
+                        if 'total_chocolate' in profile['events']['easter']:
+                            if profile['events']['easter']['total_chocolate'] > chocolate:
+                                chocolate = profile['events']['easter']['total_chocolate']
+
+                        if 'rabbits' in profile['events']['easter']:
+                            dupes = 0
+
+                            for i in profile['events']['easter']['rabbits'].values():
+                                if type(i) != dict:
+                                    dupes += i - 1
+
+                            if dupes > rabbits_dupes:
+                                rabbits_dupes = dupes
 
         # Query to fetch data for specific usernames
         cursor.execute(f"""
-            INSERT INTO cf_tracker (username, chocolate, increase, timestramp)
-            VALUES ('{username}', {chocolate}, {chocolate - last_row}, CURRENT_TIMESTAMP);
+            INSERT INTO cf_tracker (username, chocolate, increase, timestramp, rabbit_dupes, rabbit_dupes_inc)
+            VALUES ('{username}', {chocolate}, {chocolate - last_row[0]}, CURRENT_TIMESTAMP, {rabbits_dupes}, {rabbits_dupes - last_row[1]});
         """)
 
     conn.commit()
