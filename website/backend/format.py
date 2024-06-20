65b65485b5613732a3b0166abf9cec4fd713cba1
Shrey <shreyjain1403@gmail.com>
2024-06-21 00:21:38 +0530

    fixed a small bug not showing decimal

--- website/backend/format.py
+++ website/backend/format.py
@@ -5,7 +5,7 @@ def db_response(vals):
 
         if val[1] == 'skyblock_xp':
             res.append([val[0], val[1], val[9], val[2], val[3], '{0:,}'.format(val[4]/100),
-                        val[5], '{0:,}'.format(val[6]), val[7], '{0:,}'.format(val[8]/100)])
+                        val[5], '{0:,}'.format(val[6]/100), val[7], '{0:,}'.format(val[8]/100)])
         else:
             res.append([val[0], val[1], val[9], val[2], val[3], '{0:,}'.format(val[4]),
                         val[5], '{0:,}'.format(val[6]), val[7], '{0:,}'.format(val[8])])
